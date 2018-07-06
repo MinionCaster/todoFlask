db6c835a6c0939e5510b56ba6ade1f0c8f6ef731
Psych00l <35641863+Psych00l@users.noreply.github.com>
2018-07-06 15:11:50 +0200

    added app.py with server storage of notes

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, session
+from flask_session import Session
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+notes = []
+
+@app.route("/", method="POST")
+def index():
+    note = request.form.get("note")
+    notes.append(note)
+
+    return render_template("index.html", notes=notes)
