e5c2ec9fdcae51853d66c566bd7138e9f376aa91
Psych00l <35641863+Psych00l@users.noreply.github.com>
2018-07-06 15:29:52 +0200

    changed session to be local, not server

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, request, session
 from flask_session import Session
+
 app = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-notes = []
-
-@app.route("/", method="POST")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    note = request.form.get("note")
-    notes.append(note)
+    if session.get("notes") is None:
+        session["notes"] = []
+    if request.method == "POST":
+        note = request.form.get("note")
+        session["notes"].append(note)
 
-    return render_template("index.html", notes=notes)
+    return render_template("index.html", notes=session["notes"])
